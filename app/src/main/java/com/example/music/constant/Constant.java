3bbcaac7c6377c581e4804da3f775bc9439f38f0
MeiAhaithaml <nguyenhaibxlc74@gmail.com>
2024-04-15 10:00:45 +0700

    BTN6

---
+++ app/src/main/java/com/example/music/constant/Constant.java
@@ -0,0 +1,33 @@
+package com.example.music.constant;
+
+public class Constant {
+
+    public static final String GENERIC_ERROR = "General error, please try again later";
+
+    public static final String PAGE_FACEBOOK = "https://www.facebook.com/ktqdNEU";
+    public static final String LINK_FACEBOOK = "https://www.facebook.com/ktqdNEU";
+    public static final String LINK_YOUTUBE = "https://www.facebook.com/ktqdNEU";
+    public static final String PHONE_NUMBER = "+0333144559";
+    public static final String GMAIL = "NEU@gmail.com";
+    public static final String SKYPE_ID = "";
+    public static final String ZALO_LINK = "https://www.facebook.com/ktqdNEU";
+
+    // Firebase url
+    public static final String FIREBASE_URL = "https://musicapp-8ffd0-default-rtdb.firebaseio.com";
+
+
+    public static final int MAX_COUNT_BANNER = 5;
+    public static final int MAX_COUNT_POPULAR = 4;
+    public static final int MAX_COUNT_LATEST = 4;
+
+    // Music actions
+    public static final int PLAY = 0;
+    public static final int PAUSE = 1;
+    public static final int NEXT = 2;
+    public static final int PREVIOUS = 3;
+    public static final int RESUME = 4;
+    public static final int CANNEL_NOTIFICATION = 5;
+    public static final String MUSIC_ACTION = "musicAction";
+    public static final String SONG_POSITION = "songPosition";
+    public static final String CHANGE_LISTENER = "change_listener";
+}
